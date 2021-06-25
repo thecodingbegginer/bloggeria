6caf5576ab4971963850c3a74c3e991e40d5ed90
thecodingbegginer <84171188+thecodingbegginer@users.noreply.github.com>
2021-06-25 14:45:52 +0530

    Study Page

---
+++ pages/study.js
@@ -0,0 +1,15 @@
+
+let music = new Audio('../assets/music/music.mp3');
+function play() {
+    music.play();
+    music.muted = false;
+    let disc = document.getElementById('dj');
+    disc.style.animationName = 'spinner';
+
+}
+function pause() {
+    music.pause();
+    let disc = document.getElementById('dj');
+    disc.style.animationName = '';
+}
+
