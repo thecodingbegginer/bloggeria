8a795df8be1b7d3660ce6e978912b8d348fae8c3
thecodingbegginer <84171188+thecodingbegginer@users.noreply.github.com>
2021-06-25 15:33:55 +0530

    StudyPage  changes

--- pages/study.js
+++ pages/study.js
@@ -1,15 +1,13 @@
+var music = new Audio('..\assets\music\music.mp3');
 
-let music = new Audio('../assets/music/music.mp3');
 function play() {
     music.play();
     music.muted = false;
-    let disc = document.getElementById('dj');
-    disc.style.animationName = 'spinner';
-
+    let disc = document.getElementById('dj')
+    disc.style.animationName = ('spinner');
 }
 function pause() {
     music.pause();
-    let disc = document.getElementById('dj');
-    disc.style.animationName = '';
+    let disc = document.getElementById('dj')
+    disc.style.animationName = ('');
 }
-
