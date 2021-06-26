f9f218fbc14fb53a41111d6ce09ff635925871a5
thecodingbegginer <84171188+thecodingbegginer@users.noreply.github.com>
2021-06-26 12:45:27 +0530

    Changed Link Mediafire

--- pages/study.js
+++ pages/study.js
@@ -1,4 +1,4 @@
-var music = new Audio('https://github.com/thecodingbegginer/bloggeria/raw/main/assets/music/1.mp3')
+var music = new Audio('https://download939.mediafire.com/t2e3fx0utodg/44jo88rjsq3o4rr/Website+Music.mp3')
 function play() {
     music.play();
     music.muted = false;
